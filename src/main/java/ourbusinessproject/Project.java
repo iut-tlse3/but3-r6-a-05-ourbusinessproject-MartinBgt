bfae80a87bf6c274d38fb2c96d08cb16e16c04a3
MartinBgt <martin.bagot@hotmail.com>
2026-01-27 10:35:29 +0100

    fix #1.1 classe Project

---
+++ src/main/java/ourbusinessproject/Project.java
@@ -0,0 +1,28 @@
+package ourbusinessproject;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Project {
+
+    @NotBlank
+    private String title;
+    private String description;
+
+    public Project(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Project() {
+        this.title = "default title";
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
